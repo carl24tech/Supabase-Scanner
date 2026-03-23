b035b71fffe49c45f59e32d396fd82f91bdd1da0
XTR Softwares 🕵️ <cwilly403@gmail.com>
2026-03-24 01:42:49 +0300

    @carl24tech

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-##Fork this repository, Fill this files with your Supabase credentials and run the tool on Kali,Windows PS, CMD
+##After you Fork this repository, Fill this fields with your Supabase credentials and run the tool on Kali,Windows PS, CMD
 SUPABASE_URL = "https://your-project-ref.supabase.co"
 ANON_KEY = "your-anon-key-here"
 SERVICE_ROLE_KEY = ""
